742f11c8d4416f4b1690f86e9878c5a5edfd1578
israelopeters <israelopeters@gmail.com>
2024-05-13 11:44:40 +0100

    feat: adding first example

--- src/main/java/examples/AsynchronousExamples.java
+++ src/main/java/examples/AsynchronousExamples.java
@@ -1,7 +1,18 @@
 package examples;
 
+import java.util.concurrent.CompletableFuture;
+
 public class AsynchronousExamples {
     public static void main(String[] args) {
+
+//        for (int i = 0; i < 20; i++) {
+//            System.out.println("Main thread running...");
+//        }
+
+//        CompletableFuture<Void> futurePrint =
+//                CompletableFuture.runAsync(
+//                        () -> System.out.println("Hello World!")
+//                );
 
     }
 }
